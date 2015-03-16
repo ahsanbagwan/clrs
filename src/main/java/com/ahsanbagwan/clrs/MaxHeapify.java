f3b738f7974662fc9021053fa7ec5be8e060ac91
Ahsan Bagwan <ahsanbagwan@gmail.com>
2015-03-16 16:49:31 +0530

    Initial commit

---
+++ src/main/java/com/ahsanbagwan/clrs/MaxHeapify.java
@@ -0,0 +1,16 @@
+package com.ahsanbagwan.clrs;
+
+public class MaxHeapify {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] array = {16, 4, 10, 14, 7, 9, 3, 2, 8, 1};
+		int heapSize = array.length;
+		
+	}
+	
+	public static void maxHeapify(int[] array, int i) {
+		
+	}
+}
